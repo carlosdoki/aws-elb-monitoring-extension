919b9f407f16c41b478223b4920f73d80991bfb0
Bhuvnesh Kumar <raghur171@gmail.com>
2018-07-17 16:17:24 -0700

    dashboard test 2

--- src/test/java/com/appdynamics/extensions/aws/elb/DashboardTest.java
+++ src/test/java/com/appdynamics/extensions/aws/elb/DashboardTest.java
@@ -8,8 +8,61 @@
 
 package com.appdynamics.extensions.aws.elb;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import com.appdynamics.extensions.dashboard.CustomDashboardJsonUploader;
+import org.mockito.Mockito;
+import sun.print.CUPSPrinter;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by bhuvnesh.kumar on 7/17/18.
  */
 public class DashboardTest {
+
+    private static final Logger logger = Logger.getLogger(DashboardTest.class);
+
+    private Map config;
+    private String dashboardJson ;
+
+    private void valueMap(){
+        config = new HashMap();
+        config.put("enabled","true");
+        config.put("namePrefix","Prefix");
+        config.put("uploadDashboard","true");
+        config.put("executionFrequencyMinutes","5");
+        config.put("host","localhost");
+        config.put("port","8090");
+        config.put("account","customer");
+        config.put("username","user");
+        config.put("password","pass");
+        config.put("applicationName","app");
+        config.put("tierName","tier");
+        config.put("nodeName","node");
+    }
+
+    private void getJsonAsString()  {
+        try {
+            dashboardJson = FileUtils.readFileToString(new File("src/test/resources/conf/dashboard.json"));
+
+        } catch (Exception e){
+            logger.error("Error in file reading");
+        }
+    }
+
+    @Test
+    public void testDashboard(){
+        valueMap();
+        getJsonAsString();
+        Dashboard dashboard = new Dashboard(config, dashboardJson);
+        CustomDashboardJsonUploader customDashboardJsonUploader = new CustomDashboardJsonUploader();
+        CustomDashboardJsonUploader customSpy = Mockito.spy(customDashboardJsonUploader);
+        Mockito.doNothing().when(customSpy).uploadDashboard(config.get("namePrefix").toString(), dashboardJson, config, false);
+        dashboard.sendDashboard();
+    }
 }
